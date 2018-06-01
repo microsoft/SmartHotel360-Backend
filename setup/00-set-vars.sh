a7725d02fd622d410a435f33baa448a9c9fb266b
eiximenis <etomas@gmail.com>
2018-06-01 14:18:44 +0200

    updated set-vars

--- setup/00-set-vars.sh
+++ setup/00-set-vars.sh
@@ -1,9 +1,69 @@
 #!/bin/bash
 
-export AKS_SUB=<your subscription id>
-export AKS_RG=<your resource group name>
-export AKS_NAME=<your cluster name>
-export ACR_NAME=<your registry name>
-export AKS_REGION=eastus
-export SPN_CLIENT_ID=<service principal app id>
-export SPN_PW=<service principal password>
+aks_sub=
+aks_rg=
+aks_name=
+acr_name=
+aks_region=eastus
+spn_client_id=
+spn_pw=
+
+
+usage() {
+   echo "Usage: 00-set-vars.sh  -p value (or --parameter value). Parameters are:"
+   echo "   -g (--resource-group): Your resource group name"
+   echo "   -s (--subscripion): Subscription id"
+   echo "   -n (--name): Cluster name"
+   echo "   -r (--registry): ACR name"
+   echo "   -l (--location): Location (defaults to eastus)"
+   echo "   -c (--spn-client): Service principal app id"
+   echo "   -p (--spn-pwd): Service principal pwd"
+}
+
+
+if [ "$1" == "" ]
+then
+  usage
+  exit 1
+fi
+
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -g | --resoure-group)           shift
+                                	aks_rg=$1
+                                	;;
+	-s | --subscription)		shift
+					aks_sub=$1
+					;;
+	-n | --name)			shift
+					aks_name=$1
+					;;
+	-r | --registry)		shift
+					acr_name=$1
+					;;
+        -l | --location)                shift
+					aks_region=$1
+					;;
+	-c | --spn-client)		shift
+					spn_client_id=$1
+					;;
+	-p | --spn-pwd)			shift
+					spn_pw=$1
+					;;
+        -h | --help)			shift
+					usage
+					;;
+        * )                     	usage
+                                	exit 1
+    esac
+    shift
+done
+
+export AKS_SUB=$aks_sub
+export AKS_RG=$aks_rg
+export AKS_NAME=$aks_name
+export ACR_NAME=$acr_name
+export AKS_REGION=$aks_region
+export SPN_CLIENT_ID=$spn_client_id
+export SPN_PW=$spn_pw
