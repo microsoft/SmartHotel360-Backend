51745f815e9acbf361864811735249173bd80432
eiximenis <etomas@gmail.com>
2018-06-08 19:00:03 +0200

    new scripts

--- setup/00-set-vars.sh
+++ setup/00-set-vars.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 aks_sub=
 aks_rg=
@@ -7,7 +7,7 @@ acr_name=
 aks_region=eastus
 spn_client_id=
 spn_pw=
-
+sh360_appname=myapp
 
 usage() {
    echo "Usage: 00-set-vars.sh  -p value (or --parameter value). Parameters are:"
@@ -18,6 +18,7 @@ usage() {
    echo "   -l (--location): Location (defaults to eastus)"
    echo "   -c (--spn-client): Service principal app id"
    echo "   -p (--spn-pwd): Service principal pwd"
+   echo "   -a (--app-name): Name of the sh360 application to install (defaults to myapp)"
 }
 
 
@@ -30,33 +31,36 @@ fi
 
 while [ "$1" != "" ]; do
     case $1 in
-        -g | --resoure-group)           shift
-                                	aks_rg=$1
-                                	;;
-	-s | --subscription)		shift
-					aks_sub=$1
-					;;
-	-n | --name)			shift
-					aks_name=$1
-					;;
-	-r | --registry)		shift
-					acr_name=$1
-					;;
-        -l | --location)                shift
-					aks_region=$1
-					;;
-	-c | --spn-client)		shift
-					spn_client_id=$1
-					;;
-	-p | --spn-pwd)			shift
-					spn_pw=$1
-					;;
-        -h | --help)			shift
-					usage
-					;;
-        * )                     	usage
-                                	exit 1
-    esac
+      -g | --resoure-group)   shift
+                              aks_rg=$1
+                              ;;
+      -s | --subscription)    shift
+                              aks_sub=$1
+                              ;;
+      -n | --name)            shift
+                              aks_name=$1
+                              ;;
+      -r | --registry)            shift
+                              acr_name=$1
+                              ;;
+      -l | --location)            shift
+                              aks_region=$1
+                              ;;
+      -c | --spn-client)      shift
+                              spn_client_id=$1
+                              ;;
+      -p | --spn-pwd)         shift
+                              spn_pw=$1
+                              ;;
+      -h | --help)            shift
+                              usage
+                              ;;
+      -a | --app-name)        shift
+                              sh360_appname=$1
+                              ;;
+      * )                     usage
+                              exit 1
+      esac
     shift
 done
 
@@ -67,3 +71,4 @@ export ACR_NAME=$acr_name
 export AKS_REGION=$aks_region
 export SPN_CLIENT_ID=$spn_client_id
 export SPN_PW=$spn_pw
+export SH360_APPNAME=$sh360_appname
--- setup/02-deploy-apis.sh
+++ setup/02-deploy-apis.sh
@@ -1,35 +1,78 @@
 #!/bin/bash
 
-cd ../src/SmartHotel360-Azure-backend/deploy/k8s
+registry=
+acrName=${ACR_NAME}
+imageTag=$(git rev-parse --abbrev-ref HEAD)
+dockerOrg="smarthotels"
+appName=${SH360_APPNAME}
+
+
+if [[ "$acrName" == "" ]]
+then
+  registry=$acrName.azurecr.io
+fi
+
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -r | --registry)                shift
+                                        registry=$1
+                                        ;;
+        -t | --tag)                     shift
+                                        imageTag=$1
+                                        ;;
+        -o | --org)                     shift
+                                        dockerOrg=$1
+                                        ;;
+        -n | --name)                    shift
+                                        appName=$1
+                                        ;;
+        --release)                      shift
+                                        release=$1
+                                        ;;
+       * )                              echo "Invalid param. Use mandatory -n (or --name)"
+                                        echo "Optionals -c (--clean), -r (--registry), -o (--org) or -t (--tag), --release"
+                                        exit 1
+    esac
+    shift
+done
+
+pushd ../src/SmartHotel360-Azure-backend/deploy/k8s
+
+
+if [[ "$acrName" != "" ]]
+then
+  echo "------------------------------------------------------------"
+  echo "Creating the registry $acrName"
+  echo "------------------------------------------------------------"
+  az acr create -n $acrName -g ${AKS_RG} --admin-enabled --sku Basic
+  echo "------------------------------------------------------------"
+  echo "Building and pushing to $registry"
+  echo "------------------------------------------------------------"
+  . build-push.sh $acrName
+fi
 
 echo "------------------------------------------------------------"
-echo "Creating the registry ${ACR_NAME}"
+echo "Deploying the code from registry '$acrName'"
+echo "Application Name used: $appName"
+echo "Image tag is: $imageTag"
 echo "------------------------------------------------------------"
-az acr create -n ${ACR_NAME} -g ${AKS_RG} --admin-enabled --sku Basic
 
-echo "------------------------------------------------------------"
-echo "Applying PostgreSQL databases"
-echo "------------------------------------------------------------"
-kubectl apply -f postgres.yaml
+if [[ "$acrName" != "" ]]
+then
+  . deploy.sh  --registry $registry --release $appName -n $appName -t $imageTag
+else
+  . deploy.sh  --release $appName -n $appName -t $imageTag
+fi
 
-echo "------------------------------------------------------------"
-echo "Applying SQL Server databases"
-echo "------------------------------------------------------------"
-kubectl apply -f sql-data.yaml
 
-echo "------------------------------------------------------------"
-echo "Building and pushing to ACR"
-echo "------------------------------------------------------------"
-. build-push.sh ${ACR_NAME}
+if [[ "${AKS_NAME}" != "" ]]
+then
 
-echo "------------------------------------------------------------"
-echo "Deploying the code"
-echo "------------------------------------------------------------"
-. deploy.sh ${ACR_NAME}.azurecr.io
+  echo "------------------------------------------------------------"
+  echo "Opening the dashboard"
+  echo "------------------------------------------------------------"
+  az aks browse -n ${AKS_NAME} -g ${AKS_RG}
+fi
 
-echo "------------------------------------------------------------"
-echo "Opening the dashboard"
-echo "------------------------------------------------------------"
-az aks browse -n ${AKS_NAME} -g ${AKS_RG}
-
-cd ../../../../setup
+popd ../../../../setup
