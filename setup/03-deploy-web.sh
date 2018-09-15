c1555cdb6b7b6a789f23de8a81f142c9d484e04b
eiximenis <etomas@gmail.com>
2018-09-15 11:43:34 +0200

    missing values file for web

--- setup/03-deploy-web.sh
+++ setup/03-deploy-web.sh
@@ -192,7 +192,7 @@ fullrelease="$release-publicweb"
 
 echo "Installing publicweb ($currentImage)"
 
-echo "helm install sh360-web --name=$fullrelease --set image.tag=$imageTag --set image.repository=$currentImage --set appName=$appName --set ingress.enabled=1 --set ingress.hosts={$dns} -f ingress_values.yml -f pull_secrets_conf.yml -f infrastructure_values.yml"
-helm install sh360-web --name=$fullrelease --set image.tag=$imageTag --set image.repository=$currentImage --set appName=$appName --set ingress.enabled=1 --set ingress.hosts={$dns} -f ingress_values.yml -f pull_secrets_conf.yml -f infrastructure_values.yml
+echo "helm install sh360-web --name=$fullrelease --set image.tag=$imageTag --set image.repository=$currentImage --set appName=$appName --set ingress.enabled=1 --set ingress.hosts={$dns} -f ingress_values.yml -f pull_secrets_conf.yml -f infrastructure_values.yml -f web_values.yml"
+helm install sh360-web --name=$fullrelease --set image.tag=$imageTag --set image.repository=$currentImage --set appName=$appName --set ingress.enabled=1 --set ingress.hosts={$dns} -f ingress_values.yml -f pull_secrets_conf.yml -f infrastructure_values.yml -f web_values.yml
 
 popd
